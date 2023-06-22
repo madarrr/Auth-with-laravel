2087c478ac79acbe76f2919fc28cbb4d0cc9888d
madarrr <delaureaka@gmail.com>
2023-06-22 17:57:23 +0000

    update of authentification view

---
+++ resources/views/auth.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Authentification | Home</title>
+</head>
+<body class="big-bg-design">
+  <section>
+    <form action="/" class="container" method="post">
+           <div class="card-design" style="float:right" id=style-bg>
+           <img src="/images/authi-bg.png" alt="" class="bg-resize col-sm-1" style="float:left">
+                <div class="imgmove">
+                    <img src="/images/auth.png" alt="" id="resize">
+                    <h3>Login</h3>
+                </div>
+                    <div class="mb-3">
+                    <input type="email" class="col-md-3 col-sm-1" id="exampleInputEmail1"  placeholder="Username">
+                </div>
+                <div class="mb-3">
+                    <input type="password" class="col-md-3 col-sm-1" id="exampleInputPassword1" placeholder="Password" >
+                </div> 
+                <div class="mb-3">
+                    <button type="submit" class="btn btn-primary col-sm-1" id="buttonstyle">Sign in</button> 
+                </div> 
+           </div>
+           <p>&copy; 2022 github.com/madarrr</p>
+    </form>
+  </section>
+            
+</body>
+
+
+</html>
