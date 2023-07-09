db841cd4cc4f2aa2edc2d068f0565001c245284a
madarrr <delaureaka@gmail.com>
2023-07-09 23:17:27 +0000

    adding column isAdmin for users role

---
+++ database/migrations/2023_07_09_225943_add_role_to_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //adding role field can be null and comment to said if value off table is 1 mean table is admin
+            $table->boolean('is_admin')->nullable()->comment('1 - admin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropColumn('is_admin');
+        });
+    }
+};
